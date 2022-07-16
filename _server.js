bb793367e5938a70bc00dd816737da9c14f8a4f4
Robotics <robotics@umeprep.org>
2022-07-16 14:08:04 -0500

    Adds multiplayer scalability and functionality using sockets

---
+++ _server.js
@@ -0,0 +1,135 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//Standard file-serving
+const server = http.createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    let filename = '.' + q.pathname;
+
+    if (filename=='.') {
+        filename = './index.html';//Default to index.html
+    }
+    if (filename.lastIndexOf('/') >= filename.length - 1) {
+        filename += 'index.html';//Only a directory? Default to index.html of that directory
+    }
+    if (filename.lastIndexOf('.') < filename.lastIndexOf('/')) {
+        filename += '.html';//No file ending? Default to .html
+    }
+
+    let ext = path.parse(filename).ext;
+    // maps file extension to MIME typere
+    let MIME_TYPE = {
+        '.ico': 'image/png',
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.json': 'application/json',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.wav': 'audio/wav',
+        '.mp3': 'audio/mpeg',
+        '.svg': 'image/svg+xml',
+        '.pdf': 'application/pdf',
+        '.doc': 'application/msword'
+    };
+
+    fs.readFile(filename, function(err, data) {
+      if (err || filename.indexOf('_') != -1) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': MIME_TYPE[ext] || 'text/plain'});
+      res.write(data);
+      return res.end();
+    });
+});
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+console.log("Listening on port 8080");
+
+
+//SOCKETS
+const io = require('socket.io')(server);
+const SOCKET_LIST = {};
+const players = {};
+const rooms = [];
+const PLAYER_TYPE = {HUMAN: 0,ROBOT: 1,AI: 2,H: 0,R: 1};
+const SUIT = {0: 'Spades',1: 'Clubs',2: 'Hearts',3: 'Diamonds',4: 'Trump'};
+const RED_VALUE = {0: 'Ace',1: 'Two',2: 'Three',3: 'Four',4: 'Jack',5: 'Rider',6: 'Queen',7: 'King'};
+const BLACK_VALUE = {0: 'Seven',1: 'Eight',2: 'Nine',3: 'Ten',4: 'Jack',5: 'Rider',6: 'Queen',7: 'King'};
+const TRUMP_VALUE = {0: 'I', 1: 'II', 2: 'III', 3: 'IIII', 4: 'V', 5: 'VI', 6: 'VII', 7: 'VIII', 8: 'IX', 9: 'X', 10: 'XI', 11: 'XII', 12: 'XIII', 13: 'XIV', 14: 'XV', 15: 'XVI', 16: 'XVII', 17: 'XVIII', 18: 'XIX', 19: 'XX', 20: 'XXI', 21: 'Skyz'};
+let baseDeck = [];
+for (let s=0;s<4;s++)
+    for (let v=0;v<8;v++)
+        baseDeck.push({'value': s > 1 ? RED_VALUE[v] : BLACK_VALUE[v] ,'suit':SUIT[s]});
+for (let v=0;v<22;v++)
+    baseDeck.push({'value':TRUMP_VALUE[v],'suit':SUIT[4]});
+function Player(type) {this.type = type;this.socket = -1;this.pid = -1;this.chips = 100;this.discard = [];this.hand = [];}
+
+io.sockets.on('connection', function(socket) {
+    let socketId = Math.random()*100000000000000000;
+    SOCKET_LIST[socketId] = socket;
+
+    players[socketId] = {'id':socketId,'pid':-1,'room':-1,'socket':socket};
+
+    socket.on('instanceCheck', function(playerID) {
+        for (let i in players) {
+            if (players[i].pid == playerID)
+                socket.disconnect();
+        }
+        players[socketId].pid = playerID;
+    });
+
+    socket.on('disconnect', function() {
+        delete players[socketId];
+        delete SOCKET_LIST[socketId];
+    });
+
+    socket.on('roomConnect', function(roomNumber) {
+        let connected = false;
+        if (rooms[roomNumber] && rooms[roomNumber][playerCount] < 4) {
+            for (let i=0;i<4;i++) {
+                if (rooms[roomNumber][players][i].type == PLAYER_TYPE.ROBOT) {
+                    rooms[roomNumber][players][i].type = PLAYER_TYPE.HUMAN;
+                    rooms[roomNumber][players][i].socket = socketId;
+                    rooms[roomNumber][players][i].pid = players[socketId].pid;
+                    socket.emit('roomConnected', roomNumber);
+                    connected = true;
+                    players[socketId]['room'] = roomNumber;
+                    break;
+                }
+            }
+        }
+        if (!connected) socket.emit('roomNotConnected',roomNumber);
+    });
+});
+
+function tick() {
+    for (let i=rooms.length-2; i>=0;i++) {
+        //Operations
+        if (rooms[i].playerCount == 0) {
+            delete rooms[i];
+        }
+    }
+    for(let i in players){
+        if (!~players[i]['room']) {
+            players[i]['socket'].emit('returnRooms',rooms);
+        }
+    }
+    if (rooms.length == 0 || rooms[rooms.length-1].playerCount > 0) {
+        rooms.push({'playerCount':0,'deck':[...baseDeck],'players':[new Player(PLAYER_TYPE.ROBOT),new Player(PLAYER_TYPE.ROBOT), new Player(PLAYER_TYPE.ROBOT), new Player(PLAYER_TYPE.ROBOT)]});
+    }
+}
+
+let interval = setInterval(tick,1000/30.0);//30 FPS
+
+//Begin listening
+server.listen(8080);
--- machTaroky.js
+++ machTaroky.js
@@ -1,43 +1,11 @@
-const PLAYER_TYPE = {
-    HUMAN: 0,
-    ROBOT: 1,
-    AI: 2,
-    H: 0,
-    R: 1
-};
-const SUIT = {
-    0: 'Spades',
-    1: 'Clubs',
-    2: 'Hears',
-    3: 'Diamonds',
-    4: 'Trump'
-};
-const RED_VALUE = {
-    0: 'Ace',
-    1: 'Two',
-    2: 'Three',
-    3: 'Four',
-    4: 'Jack',
-    5: 'Rider',
-    6: 'Queen',
-    7: 'King'
-};
-const BLACK_VALUE = {
-    0: 'Seven',
-    1: 'Eight',
-    2: 'Nine',
-    3: 'Ten',
-    4: 'Jack',
-    5: 'Rider',
-    6: 'Queen',
-    7: 'King'
-};
-const TRUMP_VALUE = {
-    0: 'I', 1: 'II', 2: 'III', 3: 'IIII', 4: 'V', 5: 'VI', 6: 'VII', 7: 'VIII', 8: 'IX', 9: 'X', 10: 'XI', 11: 'XII', 12: 'XIII', 13: 'XIV', 14: 'XV', 15: 'XVI', 16: 'XVII', 17: 'XVIII', 18: 'XIX', 19: 'XX', 20: 'XXI', 21: 'Skyz'
-};
-const refRes = {width: 1000, height: 1000};
-const WIDTH = 1000;
-const HEIGHT = 1000;
+const PLAYER_TYPE = {HUMAN: 0,ROBOT: 1,AI: 2,H: 0,R: 1};
+const SUIT = {0: 'Spades',1: 'Clubs',2: 'Hearts',3: 'Diamonds',4: 'Trump'};
+const RED_VALUE = {0: 'Ace',1: 'Two',2: 'Three',3: 'Four',4: 'Jack',5: 'Rider',6: 'Queen',7: 'King'};
+const BLACK_VALUE = {0: 'Seven',1: 'Eight',2: 'Nine',3: 'Ten',4: 'Jack',5: 'Rider',6: 'Queen',7: 'King'};
+const TRUMP_VALUE = {0: 'I', 1: 'II', 2: 'III', 3: 'IIII', 4: 'V', 5: 'VI', 6: 'VII', 7: 'VIII', 8: 'IX', 9: 'X', 10: 'XI', 11: 'XII', 12: 'XIII', 13: 'XIV', 14: 'XV', 15: 'XVI', 16: 'XVII', 17: 'XVIII', 18: 'XIX', 19: 'XX', 20: 'XXI', 21: 'Skyz'};
+const refRes = {width: 1200, height: 1200};
+const WIDTH = refRes.width;
+const HEIGHT = refRes.height;
 let ticker;
 let ctx;
 let players;
@@ -45,40 +13,57 @@ let deck;
 let canvas;
 let scale;
 let origin;
-let baseDeck = [];
-for (let s=0;s<4;s++)
-    for (let v=0;v<8;v++)
-        baseDeck.push({'value': s > 1 ? RED_VALUE[v] : BLACK_VALUE[v] ,'suit':SUIT[s]});
-for (let v=0;v<22;v++)
-    baseDeck.push({'value':TRUMP_VALUE[v],'suit':SUIT[4]});
-
-function Player(position, type) {
-    this.position = position;
-    this.type = type;
-    this.hand = [];
-    this.chips = 100;
-}
-
-function startGame() {
-    players = [];
-    players[0] = new Player(0,PLAYER_TYPE.H);
-    players[1] = new Player(1,PLAYER_TYPE.R);
-    players[2] = new Player(2,PLAYER_TYPE.R);
-    players[3] = new Player(3,PLAYER_TYPE.R);
-    deck = [...baseDeck];
-}
+let socket;
+let availableRooms;
+let inGame = false;
+let menuScroll;
 
 window.onload = () => {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    startGame();
-    ticker = setInterval(tick(), 1000/30.0);//30 FPS
+    menuScroll = 5;
+    ticker = setInterval(tick, 1000/30.0);//30 FPS
+    socket = io();
+
+    if (localStorage.getItem('tarokyInstance') == 0)
+        localStorage.setItem('tarokyInstance',Math.random());
+
+    socket.emit('instanceCheck',localStorage.getItem('tarokyInstance'));
+
+    socket.on('returnRooms', function(returnRooms) {
+        availableRooms = returnRooms;
+    });
+    socket.on('returnPlayers', function(returnPlayers) {
+        players = returnPlayers;
+    });
+    socket.on('returnHand', function(returnHand) {
+        hand = returnHand;
+    });
+    socket.on('returnDeck', function(returnDeck) {
+        deck = returnDeck;
+    });
 }
 
 function drawBoard() {
     ctx.fillStyle = 'rgb(163,123,91)';
     ctx.fillRect(0,0, WIDTH, HEIGHT);
-
+    if (inGame) {
+        //Draw board for in game
+    } else {
+        //Draw menu
+        if (availableRooms) {
+            let i=0;
+            while (i < availableRooms.length && i*200 - menuScroll < HEIGHT) {
+                //Board is 1200x1200, each room selection is 200 tall and 1100 wide. 6 can fit
+                if (menuScroll < (i+1)*200) {
+                    //Draw room
+                    ctx.fillStyle = 'brown';
+                    ctx.fillRect(50,i*200+menuScroll,1100,200);
+                }
+                i++;
+            }
+        }
+    }
 }
 
 function tick() {
