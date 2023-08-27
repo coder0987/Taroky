5e6fb286d4326e4d8f616a8d9f42186b3bd9dd30
Coder0987 <webmaster@smach.us>
2023-08-26 20:56:51 -0500

    Fixes fullscreen on Safari browsers

--- machTaroky.js
+++ machTaroky.js
@@ -131,13 +131,13 @@ function fullscreen() {
 
 //Interface with officially supported functions
 function openFullscreen() {
-  if (document.body.requestFullscreen) {
-    document.body.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    document.body.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    document.body.msRequestFullscreen();
-  }
+    if (document.body.requestFullscreen) {
+      document.body.requestFullscreen();
+    } else if (document.body.webkitRequestFullscreen) { /* Safari */
+      document.body.webkitRequestFullscreen();
+    } else if (document.body.msRequestFullscreen) { /* IE11 */
+      document.body.msRequestFullscreen();
+    }
 }
 
 /* Close fullscreen */
